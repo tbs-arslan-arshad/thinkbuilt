d26e61679d12832a86fb608c4ec342928403faf3
Arslan Arshad <Arsii0306@gmail.com>
2025-08-20 10:16:05 +0500

    nothing commit

--- src/app/(pages)/(landingPage)/components/Imagesection.tsx
+++ src/app/(pages)/(landingPage)/components/Imagesection.tsx
@@ -22,19 +22,21 @@ const Imagesection = () => {
       setAnimatedTagId(randomTagId);
     };
 
-    const intervalId = setInterval(animateTag, 1000);
+    const intervalId = setInterval(animateTag, 1200);
 
     return () => clearInterval(intervalId);
   }, []);
 
+
   return (
-    <section ref={row1Ref} className="bg-[#0E0805]">
-      <div className="flex flex-col items-center gap-[20px] my-[40.47px]  justify-center mx-4">
+     <section id="about" ref={row1Ref} className="bg-[#0E0805]">
+      <div className="flex flex-col items-center gap-4 py-[50px]  justify-center mx-4">
         <div className="grid 2xl:grid-cols-[820px_441px] xl:grid-cols-[800px_441px] gap-4 items-center justify-center ">
           <motion.div
             initial={{ opacity: 0, x: -80 }}
             animate={row1InView ? { opacity: 1, x: 0 } : { opacity: 0, x: -80 }}
-            transition={{ duration: 0.6, ease: "easeInOut" }}>
+            transition={{ duration: 0.6, ease: "easeInOut" }}
+          >
             <Image
               src={Images[0].src}
               alt={Images[0].alt}
@@ -48,7 +50,11 @@ const Imagesection = () => {
             ref={row1Ref}
             initial={{ opacity: 0, x: 80 }}
             animate={row1InView ? { opacity: 1, x: 0 } : { opacity: 0, x: 80 }}
-            transition={{ duration: 0.6, ease: "easeInOut" }}>
+            transition={{ duration: 0.6, ease: "easeInOut" }}
+          >
+
+
+
             <div className="flex flex-col bg-[#AAE3FE] p-4 rounded-3xl  2xl:h-[409] transition-all duration-500 hover-animation ">
               {FEATURES.map((stat, index) => (
                 <div key={index} className="">
@@ -74,14 +80,14 @@ const Imagesection = () => {
             animate={row2InView ? { opacity: 1, x: 0 } : { opacity: 0, x: -80 }}
             transition={{ duration: 0.6, ease: "easeInOut" }}>
             <div className="w-full xl:w-[600px] sm:h-[409px] relative flex justify-center items-center transition-all duration-500 hover-animation">
-              {/* <div className=" xl:w-[662px]  transition-all duration-500 hover-animation sm:hidden">
+              <div className=" xl:w-[662px]  transition-all duration-500 hover-animation sm:hidden">
                 <Image
                   src={Images[2].src}
                   alt={Images[2].alt}
                   width={662}
                   height={409}
                 />
-              </div> */}
+              </div>
 
               <div className="absolute   h-full hidden sm:block w-[600px] items-center justify-center text-center">
                 <Image
@@ -105,7 +111,7 @@ const Imagesection = () => {
                       tag.zIndex
                     } absolute ${
                       tag.position
-                    } transform -translate-x-1/2 hover:scale-105 transition-all duration-400
+                    } transform -translate-x-1/2 hover:scale-105 shadow-red-50/60 transition-all duration-400
             ${animatedTagId === tag.id ? "scale-110" : ""}`}>
                     <Image
                       src={tag.icon}
